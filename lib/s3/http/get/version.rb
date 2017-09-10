256343ce1f2f7ea129ce4592ca8602b9a84a2d43
abicky <takeshi.arabiki@gmail.com>
2017-09-11 05:16:09 +0900

    Initialize by "bundle gem s3-http-get --no-coc"

---
+++ lib/s3/http/get/version.rb
@@ -0,0 +1,7 @@
+module S3
+  module Http
+    module Get
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/s3/http/get_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe S3::Http::Get do
+  it "has a version number" do
+    expect(S3::Http::Get::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
